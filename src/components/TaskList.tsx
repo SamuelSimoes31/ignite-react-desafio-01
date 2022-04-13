8e6a743c4ab78aeb52acf3e098d4b9ab873ca016
Samuel Simões <samuelsimoes31@gmail.com>
2022-04-13 20:33:57 -0300

    feat: handleRemoveTask implemented

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -44,6 +44,8 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasksArray = tasks.filter(task => task.id !== id);
+    setTasks(newTasksArray);
   }
 
   return (
